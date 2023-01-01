9866b7805e4af828f1655bd0d295852b1950cd6f
Sam Richard <snugug@google.com>
2023-01-01 08:04:59 -0500

    Add testing site

---
+++ src/components/Counter.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { count } from '../counterStore';
+
+  // Starting number
+  export let start = 0;
+
+  // Set state
+  count.set(start);
+
+  const add = () => {
+    count.set($count + 1);
+  };
+  const subtract = () => {
+    count.set($count - 1);
+  };
+</script>
+
+<p>{$count}</p>
+
+<button on:click={add}>+</button>
+<button on:click={subtract}>-</button>
