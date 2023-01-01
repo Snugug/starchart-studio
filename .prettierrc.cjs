dfeb053248e41091d6b743abe18b91ec10ff531b
Sam Richard <snugug@google.com>
2023-01-01 08:04:46 -0500

    Add support for linting Svelte files

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,12 +1,21 @@
 module.exports = {
   singleQuote: true,
   trailingComma: 'all',
-  plugins: [require.resolve('prettier-plugin-astro')],
+  plugins: [
+    require.resolve('prettier-plugin-astro'),
+    require.resolve('prettier-plugin-svelte'),
+  ],
   overrides: [
     {
       files: '*.astro',
       options: {
         parser: 'astro',
+      },
+    },
+    {
+      files: '*.svelte',
+      options: {
+        parser: 'svelte',
       },
     },
   ],
