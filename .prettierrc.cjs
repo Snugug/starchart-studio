7f65b780d02b5c04cdd527eaa01ca83b859c0e46
Sam Richard <snugug@google.com>
2023-01-01 07:33:21 -0500

    Lint and node config

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
